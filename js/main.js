172bc1a7daee16528020d8c83d32c468feb7ec11
jorgeazapata <jorgeandresz8910@hotmail.com>
2024-11-03 22:10:27 -0500

    realnumber

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const numeroTelefono = "tel:3123456789"; // Número de teléfono celular predefinido
+    const numeroTelefono = "tel:3156532881"; // Número de teléfono celular predefinido
 
     const callBtn = document.getElementById('call-btn');
     callBtn.addEventListener('click', function() {
